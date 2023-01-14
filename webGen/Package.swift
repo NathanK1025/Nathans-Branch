b8d4d61916009f89b16bc866e508302a659bf487
Sparo <abel249733@gmail.com>
2023-01-13 22:14:25 -0600

    bro whatgit add -A

--- webGen/Package.swift
+++ webGen/Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "webGen",
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.4")
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- webGen/Sources/webGen/webGen.swift
+++ webGen/Sources/webGen/webGen.swift
@@ -1,18 +1,8 @@
-import ArgumentParser
-import Foundation
-struct cli: ParsableCommand {
-    static let configuration = CommandConfiguration(
-        abstract: "A Swift CLI to say names.",
-        version: "0.0.1"
-    )
-    @Argument
-    var name: String
-    @Flag(help: "Whether to say the name capitalized.")
-    var capitalized: Bool = false
-    // MARK: - Init
-    func run() throws {
-      let strName = capitalized ? name.capitalized : name
-      print("Hello, \(strName)!")
+@main
+public struct webGen {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(webGen().text)
     }
 }
-cli.main()
