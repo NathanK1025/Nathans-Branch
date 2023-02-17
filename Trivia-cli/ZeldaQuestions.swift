848a7bca8994294157a2bf22ed15d0f03873802c
NathanK1025 <111088168+NathanK1025@users.noreply.github.com>
2023-02-17 15:03:17 -0600

    Create ZeldaQuestions.swift

---
+++ Trivia-cli/ZeldaQuestions.swift
@@ -0,0 +1,14 @@
+let zelda-q: [string] = [
+  "The main character of the Legend of Zelda series is",
+  "The main villian of the Legend of Zelda series is",
+  "Ganon wields the Triforce of",
+  "Link wields the Triforce of",
+  "Zelda wields the Triforce of",
+  "Link's boat in Wind Waker is named",
+  "Link's sword is called the",
+  "Links fairy in Ocarina of Time is named",
+  "The spirit of the Master Sword's name is",
+  "The location in Link's Awakening is called",
+  "The main location of the Legend of Zelda is",
+  "The Main godess in The Legend of Zelda is",
+  
