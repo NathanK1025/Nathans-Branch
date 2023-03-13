9bfba6329cc81964c9bbd8f583bb5a1f82ef9411
NathanK1025 <111088168+NathanK1025@users.noreply.github.com>
2023-03-13 15:29:16 -0500

    Update ZeldaQuestions.swift

--- Trivia-cli/ZeldaQuestions.swift
+++ Trivia-cli/ZeldaQuestions.swift
@@ -1,14 +1,28 @@
-let zelda-q: [string] = [
-  "The main character of the Legend of Zelda series is",
-  "The main villian of the Legend of Zelda series is",
-  "Ganon wields the Triforce of",
-  "Link wields the Triforce of",
-  "Zelda wields the Triforce of",
-  "Link's boat in Wind Waker is named",
-  "Link's sword is called the",
-  "Links fairy in Ocarina of Time is named",
-  "The spirit of the Master Sword's name is",
-  "The location in Link's Awakening is called",
-  "The main location of the Legend of Zelda is",
-  "The Main godess in The Legend of Zelda is",
-  "
+import Foundation
+import Swift
+import Glibc
+mutating func shuffle()
+let zelda: [String:String] = [
+  "Link":"The main character of the Legend of Zelda series is",
+  "Ganondorf":"The main villian of the Legend of Zelda series is",
+  "Power":"Ganondorf wields the Triforce of",
+  "Courage":"Link wields the Triforce of",
+  "Wisdom":"Zelda wields the Triforce of",
+  "King of Red Lions":"Link's boat in Wind Waker is named",
+  "Master Sword":"Link's sword is called the",
+  "Navi":"Links fairy in Ocarina of Time is named",
+  "Fi":"The spirit of the Master Sword's name is",
+  "Koholint Island":"The location in Link's Awakening is called",
+  "Hyrule":"The main location of the Legend of Zelda is",
+  "Hylia":"The Main godess in The Legend of Zelda is",
+  "Minish":"The real name of the Picori are",
+  "Zora":"The race of fish people are called the",
+  "Rito":"The race of bird people are called the",
+  "Gorons":"The race of rock people are called the",
+  "Hylians":"The race of human people are called the",
+  "Gerudo":"The race of desert people are called the",
+  "Termina":"Where does Majora's Mask take place?",
+  "Demise":"The first villan chronologically is",
+  "Epona":"Link's Horse is called",
+  "Rupees":"Hylian currency is called",
+  "King Rhoam":"The last king of Hyrule in Breath of the Wild",
